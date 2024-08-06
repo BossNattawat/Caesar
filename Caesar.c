389c66c11ccb4544692f79dd9e3ad6c2c042db53
boss <75540581+Boss22341231@users.noreply.github.com>
2024-08-06 20:43:52 +0700

    first commit

---
+++ Caesar.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void){
+
+    char plaintext[200];
+    int cipher;
+
+    printf("Enter some text : ");
+    gets(plaintext);
+
+    printf("Enter a number to cipher : ");
+    scanf("%d", &cipher);
+
+    printf("Ciphertext : ");
+    for(int i = 0; i < strlen(plaintext); i++){
+        if(isupper(plaintext[i])){
+            printf("%c", (plaintext[i] - 65 + cipher) % 26 + 65);
+        }
+        else if(islower(plaintext[i])){
+            printf("%c", (plaintext[i] - 97 + cipher) % 26 + 97);
+        }
+        else{
+            printf("%c", plaintext[i]);
+        }
+    }
+    printf("\n");
+
+}
